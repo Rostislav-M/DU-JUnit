5ed68797b7d21b9237a8a944d4a39d68c04aa3cb
Rostislav-M <rostislav.malatak@gmail.com>
2024-12-03 16:37:19 +0100

    Initial commit

---
+++ src/main/java/com/engeto/Find.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.List;
+
+public class Find {
+    public static Double findMatching(List<Double> list, Double lowerLimit, Double upperLimit) {
+        return list.get(0);
+    }
+}
